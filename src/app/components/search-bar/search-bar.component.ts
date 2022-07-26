14c42a83e61b887c1a203765207a6a8a556d9d9c
Muhammad SabahAllah <musabahallah@gmail.com>
2022-07-26 17:18:49 +0200

    finish search bar styling

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    this.router.navigate(['search', form.value.search]);
+  }
 }
