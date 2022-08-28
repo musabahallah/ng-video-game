15d422590b08ff0441fa55d1848b2ac25f83e7c8
MuSabahAllah <musabahallah@gmail.com>
2022-08-28 19:56:51 +0200

    Adding Game-Details Component

---
+++ src/app/components/game-details/game-details.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
+
+@Component({
+  selector: 'app-game-details',
+  templateUrl: './game-details.component.html',
+  styleUrls: ['./game-details.component.scss'],
+})
+export class GameDetailsComponent implements OnInit {
+  gameRating = 0;
+  gameId: string = '';
+  game?: Game;
+  routeSub?: Subscription;
+  gameSub?: Subscription;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
+
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+    });
+  }
+  getGameDetails(id: string): void {
+    this.gameSub = this.httpService
+      .getGameDetails(id)
+      .subscribe((res: Game) => {
+        this.game = res;
+
+        this.gameRating = this.game?.metacritic;
+        // setTimeout(() => {
+        //   this.gameRating;
+        // }, 1000);
+      });
+  }
+
+  getColor(value: number): string {
+    if (value > 75) {
+      return '#5ee432';
+    } else if (value > 50) {
+      return '#fffa50';
+    } else if (value > 30) {
+      return '#f7aa38';
+    } else {
+      return '#ef4655';
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
+}
